16dedc4cbf25e4d785cb288b76d57cc1f6279509
DavideGit991 <pasto.davide1991@gmail.com>
2020-10-28 23:23:17 +0100

    add varie difficoltà del gioco attraverso bottoni

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-var numBombe = 16;
-var possibilitàUte = 100;
-var tentativi = possibilitàUte - numBombe;
+// var numBombe = 16;
+// var possibilitàUte = 100;
+
 var arrayNumBombe=[];
 var arrayNumUte=[];
 var punteggio = 0
+var bfacile = document.getElementById("facile");
+var bmedio = document.getElementById("medio");
+var bdifficile = document.getElementById("difficile");
 
 //genero 16 numeri diversi random tra 1 e 100 e li inserisco in un array
 while(arrayNumBombe.length < 16)
@@ -25,78 +28,82 @@ while(arrayNumBombe.length < 16)
 }
   console.log(arrayNumBombe);
 
-//chiedo all'utente di inserire un numero 100-16 volte
-for(i=1; i<=tentativi; i++)
+
+//DIFFICOLTA' GIOCO FACILE
+bfacile.addEventListener ("click",
+function()
 {
- var numeroUte = parseInt(prompt("inserisci un numero per il campo minato da 1 a 100"));
-
- //lo confronto con il mio array di bombe
-   //se non c'è e finisco il massimo numero di tentativi pusho in un arraynumeri utente e punteggio++ e l'utente ha vinto
-  if (arrayNumBombe.indexOf(numeroUte) == -1 && (arrayNumUte.length + 1) == tentativi )
-  {
-    arrayNumUte.push(numeroUte);
-    console.log("arrayUte: ", arrayNumUte);
-    console.log("hai VINTO ed hai totalizzato: ", punteggio , "punti" );
-  }
-
-  //se non c'è pusho in un arraynumeri utente e punteggio++
-  else if (arrayNumBombe.indexOf(numeroUte) == -1)
-  {
-    punteggio += 1;
-    arrayNumUte.push(numeroUte);
-    console.log("arrayUte: ", arrayNumUte);
-    console.log("Punteggio parziale: ", punteggio);
-  }
-
-  //se c'è gioco finito e stampo quanti punti o "giri" l'utente ha fatto e gli dico hai perso
-  else if (arrayNumBombe.indexOf(numeroUte) != -1)
-  {
-    console.log("hai PERSO e il tuo punteggio e': ", punteggio );
-    break;
-  }
-
+  alert('HAI 84 tentativi per battermi');
+  campoMinato(16, 100);
 }
+);
+
+//DIFFICOLTA' GIOCO MEDIA
+bmedio.addEventListener("click",
+function()
+{
+  alert('HAI 64 tentativi per battermi');
+  campoMinato(16, 80);
+}
+);
+
+//DIFFICOLTA' GIOCO DIFFICILE
+bdifficile.addEventListener("click",
+function()
+{
+  alert('HAI 34 tentativi per battermi');
+  campoMinato(16, 50);
+}
+);
 
 
 
 
 
 
-
-
-
-
-// var punteggio = 0;
-// // da fare 100 - 16 volte
-// for(i=1; i<= (100 - 16); i++)
-// {
-//   //chiedo all'utente di inserire un numero
-//   var numeroUte = parseInt(prompt("inserisci un numero per il campominato"));
-//   arrayNumUte.push(numeroUte)
-//
-//
-//     // verifico che nn sia gia' tato inserito
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//       //altrimenti l'utente ha vinto e ha totalizzato il max dei punti ovvero 100-16
-//
-//
-//
-//
-// }
 
   //funzioni
+
   //numero random
   function randomNum(num)
   {
     return Math.floor(Math.random()*num)+1;
+  }
+
+  //CAMPOMINATO
+  function campoMinato(numBombe, possibilitàUte)
+  {
+    var tentativi = possibilitàUte - numBombe;
+    //chiedo all'utente di inserire un numero 100-16 volte
+    for(i=1; i<=tentativi; i++)
+    {
+     var numeroUte = parseInt(prompt("inserisci un numero per il campo minato da 1 a 100"));
+
+     //lo confronto con il mio array di bombe
+       //se non c'è e finisco il massimo numero di tentativi pusho in un arraynumeri utente e punteggio++ e l'utente ha vinto
+      if (arrayNumBombe.indexOf(numeroUte) == -1 && (arrayNumUte.length + 1) == tentativi )
+      {
+        arrayNumUte.push(numeroUte);
+        console.log("arrayUte: ", arrayNumUte);
+        console.log("hai VINTO ed hai totalizzato: ", punteggio , "punti" );
+      }
+
+      //se non c'è pusho in un arraynumeri utente e punteggio++
+      else if (arrayNumBombe.indexOf(numeroUte) == -1)
+      {
+        punteggio += 1;
+        arrayNumUte.push(numeroUte);
+        console.log("arrayUte: ", arrayNumUte);
+        console.log("Punteggio parziale: ", punteggio);
+      }
+
+      //se c'è gioco finito e stampo quanti punti o "giri" l'utente ha fatto e gli dico hai perso
+      else if (arrayNumBombe.indexOf(numeroUte) != -1)
+      {
+        console.log("ultimo numero scelto e': ", numeroUte);
+        console.log("hai PERSO e il tuo punteggio e': ", punteggio );
+        break;
+      }
+
+    }
   }
