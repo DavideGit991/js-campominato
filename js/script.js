d369fcf0d7f27187ec5c5f9688a289cf90367796
DavideGit991 <pasto.davide1991@gmail.com>
2020-10-29 12:11:00 +0100

    stampa a video array random e araray creato dall'utente

--- js/script.js
+++ js/script.js
@@ -18,13 +18,17 @@ var bmedio = document.getElementById("medio");
 var bdifficile = document.getElementById("difficile");
 
 //genero 16 numeri diversi random tra 1 e 100 e li inserisco in un array
+var stampa1 = "";
 while(arrayNumBombe.length < 16)
 {
+
    var n = randomNum(100);
    if (arrayNumBombe.indexOf(n) == -1)
    {
     arrayNumBombe.push(n);
+
    }
+   stampa1 = stampa1 + "<li>" + arrayNumBombe[arrayNumBombe.length] + "</li>";
 }
   console.log(arrayNumBombe);
 
@@ -35,6 +39,7 @@ function()
 {
   alert('HAI 84 tentativi per battermi');
   campoMinato(16, 100);
+  document.getElementById("stampa1").innerHTML = stampa1;
 }
 );
 
@@ -44,6 +49,7 @@ function()
 {
   alert('HAI 64 tentativi per battermi');
   campoMinato(16, 80);
+  document.getElementById("stampa1").innerHTML = stampa1;
 }
 );
 
@@ -53,6 +59,7 @@ function()
 {
   alert('HAI 34 tentativi per battermi');
   campoMinato(16, 50);
+  document.getElementById("stampa1").innerHTML = stampa1;
 }
 );
 
@@ -73,6 +80,7 @@ function()
   //CAMPOMINATO
   function campoMinato(numBombe, possibilitàUte)
   {
+    var stampa2 = "";
     var tentativi = possibilitàUte - numBombe;
     //chiedo all'utente di inserire un numero 100-16 volte
     for(i=1; i<=tentativi; i++)
@@ -97,6 +105,8 @@ function()
           document.getElementById("punteggio").innerHTML = punteggio;
           document.getElementById("esito").innerHTML = "HAI VINTO E HAI TOT IL MAX PUNTEGGIO";
           alert('HAI VINTO E HAI TOT IL MAX PUNTEGGIO');
+          stampa2 = stampa2 + "<li>" + arrayNumUte[i]+ "</li>";
+          document.getElementById("stampa2").innerHTML = stampa2;
       }
 
         //se non c'è pusho in un arraynumeri utente e punteggio++
@@ -107,6 +117,8 @@ function()
           console.log("arrayUte: ", arrayNumUte);
           console.log("Punteggio parziale: ", punteggio);
           document.getElementById("punteggio").innerHTML = punteggio;
+          stampa2 = stampa2 + "<li>" + arrayNumUte[i]+ "</li>" ;
+          document.getElementById("stampa2").innerHTML = stampa2;
       }
 
         //se c'è gioco finito e stampo quanti punti o "giri" l'utente ha fatto e gli dico hai perso
@@ -115,6 +127,8 @@ function()
           console.log("ultimo numero scelto e': ", numeroUte);
           console.log("hai PERSO e il tuo punteggio e': ", punteggio );
           document.getElementById("esito").innerHTML = "HAI PERSO MI DISPIACE";
+          stampa2 = stampa2 + "<li>" + arrayNumUte[i]+ "</li>";
+          document.getElementById("stampa2").innerHTML = stampa2;
           break;
       }
 
